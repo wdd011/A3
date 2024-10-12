b1c0eadf744448c54da0bd0907933f5b91ea1789
wdd011 <zhc123001@163.com>
2024-10-13 05:59:27 +0800

    新建details界面

---
+++ A3-clientside/src/app/details/details.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common'
+import { Component, OnInit } from '@angular/core'
+import { HeaderComponent } from '../shared/header/header.component'
+import { FooterComponent } from '../shared/footer/footer.component'
+import { ActivatedRoute, Route, Router } from '@angular/router'
+import { Fundraiser, FundraisersService } from '../services/fundraisers.service'
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule, HeaderComponent, FooterComponent],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.css',
+})
+export class DetailsComponent implements OnInit {
+  constructor(private service: FundraisersService, private router: Router, private route: ActivatedRoute) {}
+  images: string[] = ['assets/img/dog.jpg', 'assets/img/yuhan.jpg', 'assets/img/yang.jpg', 'assets/img/shui.jpg', 'assets/img/dog2.jpg']
+  detailInfo: Fundraiser | undefined = undefined
+
+  ngOnInit(): void {
+    this.getDetailInfo()
+  }
+  getDetailInfo() {
+    this.service.fetchFundraiserDetails(this.route.snapshot.queryParams['id']).subscribe(res => {
+      console.log(res)
+      this.detailInfo = res
+    })
+  }
+  // 跳转
+  to(url: Route['path']) {
+    this.router.navigate([url])
+  }
+}
