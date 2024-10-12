a1a38758433c87b9c055932214082bf86fa33d6b
wdd011 <zhc123001@163.com>
2024-10-13 06:00:56 +0800

    路由新增data.menus参数

--- A3-clientside/src/app/app.routes.ts
+++ A3-clientside/src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component'
+import { DetailsComponent } from './details/details.component'
 
 export const routes: Routes = [
   {
     path: '',
     title: 'Home',
     component: HomeComponent,
+  },
+  {
+    path: 'details',
+    title: 'Details',
+    component: DetailsComponent,
+    data: {
+      menus: true, // 不显示在路由菜单上
+    },
   },
 ]
